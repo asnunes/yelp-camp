3260d4109a959c2ead502c391af335eebf752351
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2018-09-18 23:26:17 -0300

    Allow user to add campgrounds and style

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser');
 
+var campgrounds = [
+    {name: "Salmon Creek", image:"https://farm7.staticflickr.com/6139/6016438964_f6b8e1fee2.jpg"},
+    {name: "Granite Hill", image:"https://farm4.staticflickr.com/3144/2984126071_c462b62623.jpg"},
+    {name: "Montain's Rest", image:"https://farm3.staticflickr.com/2582/3820664827_6c2e9a69ae.jpg"},
+    {name: "Salmon Creek", image:"https://farm7.staticflickr.com/6139/6016438964_f6b8e1fee2.jpg"},
+    {name: "Granite Hill", image:"https://farm4.staticflickr.com/3144/2984126071_c462b62623.jpg"},
+    {name: "Montain's Rest", image:"https://farm3.staticflickr.com/2582/3820664827_6c2e9a69ae.jpg"},
+    {name: "Salmon Creek", image:"https://farm7.staticflickr.com/6139/6016438964_f6b8e1fee2.jpg"},
+    {name: "Granite Hill", image:"https://farm4.staticflickr.com/3144/2984126071_c462b62623.jpg"},
+    {name: "Montain's Rest", image:"https://farm3.staticflickr.com/2582/3820664827_6c2e9a69ae.jpg"}
+];
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); //to avoid .ejs at the end of the file
 
 app.get("/", function(req, res){
@@ -8,13 +22,20 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    campgrounds = [
-        {name: "Salmon Creek", image:"https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f7c379a2ecb2bd_340.jpg"},
-        {name: "Granite Hill", image:"https://farm4.staticflickr.com/3144/2984126071_c462b62623.jpg"},
-        {name: "Montain's Rest", image:"https://farm3.staticflickr.com/2582/3820664827_6c2e9a69ae.jpg"}
-    ];
-
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var campground = {name: name, image: image};
+
+    campgrounds.push(campground);
+    res.redirect("/campgrounds"); //default is get request
 });
 
 app.listen("3000", function(){
