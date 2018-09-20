641eebd57b7380b778a920b463e98b260e6f7ed4
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2018-09-19 23:27:17 -0300

    DB support

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs"); //to avoid .ejs at the end of the file
+
+//campground schema
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+//model from schema
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//create defaults campgrounds - doesn't work anymore
+//var campground = {name: "Salmon Creek", image:"https://farm7.staticflickr.com/6139/6016438964_f6b8e1fee2.jpg"};
+//var campground2 = {name: "Granite Hill", image:"https://farm4.staticflickr.com/3144/2984126071_c462b62623.jpg"};
+//Campground.create(campground2, newCampgroundCreated);
+
+//requests
 var campgrounds = [
     {name: "Salmon Creek", image:"https://farm7.staticflickr.com/6139/6016438964_f6b8e1fee2.jpg"},
     {name: "Granite Hill", image:"https://farm4.staticflickr.com/3144/2984126071_c462b62623.jpg"},
@@ -14,15 +34,19 @@ var campgrounds = [
     {name: "Montain's Rest", image:"https://farm3.staticflickr.com/2582/3820664827_6c2e9a69ae.jpg"}
 ];
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs"); //to avoid .ejs at the end of the file
-
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //retrive campground from database
+    Campground.find({}, function(err, campgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds", {campgrounds: campgrounds});
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
@@ -34,10 +58,23 @@ app.post("/campgrounds", function(req, res){
     var image = req.body.image;
     var campground = {name: name, image: image};
 
-    campgrounds.push(campground);
-    res.redirect("/campgrounds"); //default is get request
+    Campground.create(campground, function(err, campground){
+        createNewCampground(err, campground, res);
+    });
 });
 
+//aux functions
+function createNewCampground(err, campground, res){
+    if(err){
+        console.log(err);
+    } else{
+        console.log("A new campground has been created");
+        console.log(campground);
+        res.redirect("/campgrounds");
+    }
+}
+
+//starts server
 app.listen("3000", function(){
-    console.log("YelpCamp served stated at port 3000");
-});
+        console.log("YelpCamp served stated at port 3000");
+    });
